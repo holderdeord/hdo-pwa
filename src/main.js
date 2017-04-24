5f7168c872924d0853afefcbccf84d02cfeb1257
Arne Hassel <arne.hassel@gmail.com>
2017-04-24 03:56:10 +0200

    Added manifest

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('./service-worker.js')
-        .then(function() {
+        .then(function () {
             console.log('Service Worker Registered');
         });
 }
