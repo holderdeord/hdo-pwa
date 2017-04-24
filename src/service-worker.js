4e2f8d9301d2a8c0d0b3d1fdb25bff930cc920e6
Arne Hassel <arne.hassel@gmail.com>
2017-04-24 03:38:11 +0200

    A bit more flesh to the service-worker

--- src/service-worker.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by megoth on 24/04/2017.
- */
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
-/**
- * Created by megoth on 24/04/2017.
- */
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        watchContentBase: true
+    }
+};
